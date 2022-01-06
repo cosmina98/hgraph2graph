3863bf3bbd1d634b7add734927dfba68c337d720
Truman-Xu <ziqiaoxu@umich.edu>
2022-01-06 17:23:03 -0500

    intial tests passed

--- iter_gen.py
+++ iter_gen.py
@@ -20,25 +20,36 @@ class StockParamLoader:
         self.dropout = 0.0
 
 class _IterGenBase(HierVAE):
-    def __init__(self, batch_size, steps, model_prms, eta, seed = None):
+    def __init__(self, batch_size, steps, model_prms):
         super().__init__(model_prms)
         self.cuda()
         self.load_state_dict(torch.load(model_prms.model_loc)[0])
         self.eval()
-        if seed:
-            torch.manual_seed(seed)
+        
         self.vocab = model_prms.vocab
         self.atom_vocab = model_prms.atom_vocab
         self.batch_size = batch_size
         self.step = 0 
         self.max_step = steps
-        # multiplication factor of latent vector perturbation
-        self.eta = eta
         # Initial batch is randomly sampled
-        self.rand_sample()
+        self.z_vecs = self.rand_sample()
+        self.cur_batch = self.decode_smiles(self.z_vecs)
 
     def rand_sample(self):
-        self.z_vecs = torch.randn(self.batch_size, self.latent_size).cuda()
+        return torch.randn(self.batch_size, self.latent_size).cuda()
+
+    def perturb_latent(self, z_vecs, eta):
+        z_log_var = -torch.abs( torch.log(z_vecs.var(0)) )
+        epsilon = torch.randn_like(z_vecs).cuda()
+        return z_vecs + torch.exp(z_log_var / 2) * epsilon * eta
+
+    def encode_smiles(self, smiles_list, perturb=False):
+        # No purterbation on latent vector for the default behavior
+        _, tensorized, _ = MolGraph.tensorize(smiles_list, self.vocab, self.atom_vocab)
+        tree_tensors, graph_tensors = make_cuda(tensorized)
+        root_vecs, tree_vecs, _, graph_vecs = self.encoder(tree_tensors, graph_tensors)
+        z_vecs, _ = self.rsample(root_vecs, self.R_mean, self.R_var, perturb=perturb)
+        return z_vecs
 
     def decode_smiles(self, z_vecs):
         z_vecs_3 = (z_vecs, z_vecs, z_vecs)
@@ -60,41 +71,38 @@ class _IterGenBase(HierVAE):
             raise StopIteration
 
 class IterGenRand(_IterGenBase):
-    def __init__(self, batch_size, steps, prms, eta, seed = None):
-        super().__init__(batch_size, steps, prms, eta, seed)
+    def __init__(self, batch_size, steps, prms, eta):
+        super().__init__(batch_size, steps, prms)
 
     def generate_new_vecs(self):
         """
         Randomly sample the latent space
         """
-        self.rand_sample()
+        self.z_vecs = self.rand_sample()
 
 class IterGenDirect(_IterGenBase):
-    def __init__(self, batch_size, steps, prms, eta, seed = None):
-        super().__init__(batch_size, steps, prms, eta, seed)
+    def __init__(self, batch_size, steps, prms, eta):
+        super().__init__(batch_size, steps, prms)
+        # multiplication factor of latent vector perturbation
+        self.eta = eta
 
     def generate_new_vecs(self):
         """
         Generation is anchored on current z_vecs
         Directly perturb the current latent vectors without encoding from SMILES
         """
-        z_log_var = -torch.abs( torch.log(self.z_vecs.var(0)) )
-        epsilon = torch.randn_like(self.z_vecs).cuda()
-        self.z_vecs = self.z_vecs + torch.exp(z_log_var / 2) * epsilon * self.eta
+        self.z_vecs = self.perturb_latent(self.z_vecs, self.eta)
 
 class IterGenConvert(_IterGenBase):
-    def __init__(self, batch_size, steps, prms, eta, seed = None):
-        super().__init__(batch_size, steps, prms, eta, seed)
+    def __init__(self, batch_size, steps, prms, eta):
+        super().__init__(batch_size, steps, prms)
 
     def generate_new_vecs(self):
         """
         Generation is anchored on current SMILES batch
         Encode the current batch of SMILES and perturb the latent vectors
         """
-        _, tensorized, _ = MolGraph.tensorize(self.cur_batch, self.vocab, self.atom_vocab)
-        tree_tensors, graph_tensors = make_cuda(tensorized)
-        root_vecs, tree_vecs, _, graph_vecs = self.encoder(tree_tensors, graph_tensors)
-        self.z_vecs, _ = self.rsample(root_vecs, self.R_mean, self.R_var, perturb=True)
+        self.z_vecs=self.encode_smiles(self.cur_batch, perturb=True)
 
 if __name__=='__main__':
     # Stock ChEMBL model
@@ -102,14 +110,30 @@ if __name__=='__main__':
     vocab_loc = './data/chembl/vocab.txt'
     batch_size = 10
     steps = 20
-    seed = 42
+    torch.manual_seed(42)
     eta = 1
     prms = StockParamLoader(model_loc, vocab_loc)
 
-    batch_generator = IterGenRand(batch_size, steps, prms, eta, seed)
+    batch_generator = IterGenRand(batch_size, steps, prms, eta)
     for batch in batch_generator:
         print("Step", batch_generator.step)
         print("Total SMILES:", len(batch))
         for i, smi in enumerate(batch):
             print(i, smi)
-        
+        print('***'*10)
+
+    batch_generator = IterGenDirect(batch_size, steps, prms, eta)
+    for batch in batch_generator:
+        print("Step", batch_generator.step)
+        print("Total SMILES:", len(batch))
+        for i, smi in enumerate(batch):
+            print(i, smi)
+        print('***'*10)
+
+    batch_generator = IterGenConvert(batch_size, steps, prms, eta)
+    for batch in batch_generator:
+        print("Step", batch_generator.step)
+        print("Total SMILES:", len(batch))
+        for i, smi in enumerate(batch):
+            print(i, smi)
+        print('***'*10)
